66bab0991b41f9d5766c6d947b749b491614805d
Larissa <larissa.tardivo@estantemagica.com.br>
2024-01-31 10:29:22 -0300

    feat: set interval dates on calendar component

--- src/app/domain/entities/date-entity.ts
+++ src/app/domain/entities/date-entity.ts
@@ -3,13 +3,15 @@ export interface DayProps {
   onDayClick: (day: string) => void
   selectedDates: string[]
   allPreviousDays: string[]
-  preventedDates: string[]
   isEmptyDates: boolean
+  intervalDates: string[]
+  preventedDates: { type: string; date: string }[]
 }
 
 export interface MonthProps {
   selectedMonth: Date
   onDateChange: (selectedDates: string[]) => void
-  customDates: { type: string; date: string }[]
   isEmptyDates: boolean
+  intervalDates: string[]
+  preventedDates: { type: string; date: string }[]
 }
--- src/app/presentation/components/calendar/calendar.tsx
+++ src/app/presentation/components/calendar/calendar.tsx
@@ -1,12 +1,18 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import "./calendar-styles.scss"
 import { Month } from "./month/month"
-import { addMonths, isAfter, isSameMonth, subMonths } from "date-fns"
 import { Header } from "./header/header"
 import { Navigation } from "./navigation/navigation"
 import { Weekdays } from "./weekdays/weekdays"
+import {
+  subMonths,
+  isSameMonth,
+  isAfter,
+  addMonths,
+  format,
+} from "../../functions"
 
-export type DatePosition = "previous" | "current" | "next"
+export type DatePosition = "previous" | "next"
 export type DateAction = "clean" | "book"
 
 const customDates = [
@@ -28,19 +34,15 @@ const customDates = [
   },
 ]
 
-export function Calendar(): JSX.Element {
+export const Calendar = () => {
   const [showToast, setShowToast] = useState(false)
   const [selectedMonth, setCurrentMonth] = React.useState(new Date())
+  const [intervalDates, setIntervalDates] = React.useState<string[]>([])
+  const [preventedDates, setPreventedDates] =
+    React.useState<{ type: string; date: string }[]>(customDates)
   const [checkIn, setCheckIn] = React.useState<string | null>(null)
   const [checkOut, setCheckOut] = React.useState<string | null>(null)
   const isEmptyDates = checkIn === null && checkOut === null
-
-   const showToastMessage = () => {
-     setShowToast(true)
-     setTimeout(() => {
-       setShowToast(false)
-     }, 3000)
-   }
 
   const handleWithCalendar = (datePosition: DatePosition) => {
     const currentDate = new Date()
@@ -56,7 +58,7 @@ export function Calendar(): JSX.Element {
     }
   }
 
-  const handleDayClick = (selectedDates: string[]) => {
+  const setCheckInAndCheckout = (selectedDates: string[]) => {
     let newCheckIn: string | null = null
     let newCheckOut: string | null = null
 
@@ -71,21 +73,65 @@ export function Calendar(): JSX.Element {
     }
   }
 
-  const handleWithDates = (action: DateAction) => {
+  const formattedPreventedDates = customDates.map((item) =>
+    format(item.date, "MM-dd-yyyy")
+  )
+
+  useEffect(() => {
+    if (checkIn && checkOut) {
+      calculateAndSetIntervalDates(new Date(checkIn), new Date(checkOut))
+    }
+  }, [checkIn, checkOut])
+
+  const calculateAndSetIntervalDates = (
+    newCheckIn: Date,
+    newCheckOut: Date
+  ) => {
+    let intervalDates = []
+    let currentDate = new Date(newCheckIn)
+
+    while (currentDate <= newCheckOut) {
+      const formattedDate = format(currentDate, "MM-dd-yyyy")
+
+      if (!formattedPreventedDates.includes(formattedDate)) {
+        intervalDates.push(formattedDate)
+      }
+
+      currentDate.setDate(currentDate.getDate() + 1)
+    }
+
+    setIntervalDates(intervalDates)
+  }
+  
+  const handleWithSelectedDates = (action: DateAction) => {
     if (action === "book") {
       setCheckIn(null)
       setCheckOut(null)
       showToastMessage()
+      setPreventedDates([
+        ...preventedDates,
+        ...intervalDates.map((item) => ({ type: "booked", date: item })),
+      ])
     } else {
       setCheckIn(null)
       setCheckOut(null)
+      setIntervalDates([])
     }
   }
+
+   const showToastMessage = () => {
+     setShowToast(true)
+     setTimeout(() => {
+       setShowToast(false)
+     }, 3000)
+   }
 
   return (
     <>
       <div className={`toast-container ${showToast ? "show" : ""}`}>
-        {showToast && <div className="toast">Datas reservadas com sucesso! 😄</div>}
+        {showToast && (
+          <div className="toast">Datas reservadas com sucesso! 😄</div>
+        )}
       </div>
       <div className="calendar-container">
         <div className="calendar-title">
@@ -94,8 +140,7 @@ export function Calendar(): JSX.Element {
         <Header
           checkIn={checkIn}
           checkOut={checkOut}
-          handleWithDates={handleWithDates}
-          
+          handleWithSelectedDates={handleWithSelectedDates}
         />
         <div className="calendar-content">
           <Navigation
@@ -105,9 +150,10 @@ export function Calendar(): JSX.Element {
           <Weekdays />
           <Month
             selectedMonth={selectedMonth}
-            onDateChange={handleDayClick}
-            customDates={customDates}
+            onDateChange={setCheckInAndCheckout}
+            preventedDates={preventedDates}
             isEmptyDates={isEmptyDates}
+            intervalDates={intervalDates}
           />
         </div>
       </div>
